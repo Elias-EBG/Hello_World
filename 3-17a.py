bd171f63fce329e112c19462b38457bc5ede45b9
Elias-EBG <57652414+Elias-EBG@users.noreply.github.com>
2019-11-12 08:29:38 -0600

    No se que estoy haciendo

---
+++ 3-17a.py
@@ -0,0 +1,22 @@
+palabras = {}
+texto = input("Introduce un texto: ")
+palabra = ""
+
+for i in range(0, len(texto)):
+	if texto[i] != " ":
+		palabra += texto[i]
+
+	if texto[i] == " " or i == len(texto)-1:
+		palabras[palabra] = 0
+		print(palabra)
+		palabra = ""
+
+for i in range(0, len(texto)):
+	if texto[i] != " ":
+		palabra += texto[i]
+
+	if texto[i] == " " or i == len(texto)-1:
+		palabras[palabra] += 1
+		palabra = ""
+
+print(palabras)
